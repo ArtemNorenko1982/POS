0e9fbd3edeb7d2fe0e4b8c2dac9fc20fc4f84ab0
Artem Norenko <norenko.artem@gmail.com>
2020-11-01 22:17:56 +0200

    resolved comments

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,2 +1,2 @@
-export const isNull = (value: any) => value === null || value === undefined
-export const isStringNull = (value: string) => isNull(value) || value.trim().length === 0
+export const isNull = (value: any) => value === null || value === undefined;
+export const isStringNull = (value: string) => isNull(value) || value.trim().length === 0;
