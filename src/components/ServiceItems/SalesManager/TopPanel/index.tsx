0f5e65b5cbeffc3fb2ce2aa75648c6aeefc75537
Artem Norenko <norenko.artem@gmail.com>
2021-05-16 21:44:59 +0300

    Added base markup for TopPanel

--- src/components/ServiceItems/SalesManager/TopPanel/index.tsx
+++ src/components/ServiceItems/SalesManager/TopPanel/index.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TopPanel.scss';
 
 const TopPanel = () => {
+  const [value, handleInput] = useState('');
+
+  const inputHandler = (e) => {
+    handleInput(e.target.value);
+  };
+
   return (
     <div className="base-panel top-panel">
-      <input type="text" className="left-top-panel" />
-      <div className="right-top-panel">55</div>
+      <input
+        type="text"
+        className="left-top-panel"
+        value={value}
+        onChange={inputHandler}
+      />
+      <div className="right-top-panel">{value}</div>
     </div>
   );
 };
