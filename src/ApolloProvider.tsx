2e455fcb8b292f1325b7c227c5b6222e52ef70f5
Artem Norenko <norenko.artem@gmail.com>
2021-05-16 21:42:13 +0300

    Take out base settings

--- src/ApolloProvider.tsx
+++ src/ApolloProvider.tsx
@@ -4,11 +4,12 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
 import { Token } from './constants/LocalStorageConstants';
+import { API_URL, API_PORT } from './appConfig';
 
 const token = localStorage.getItem(Token);
 
 const client = new ApolloClient({
-  uri: 'http://localhost:15000',
+  uri: `${API_URL}:${API_PORT}`,
   cache: new InMemoryCache(),
   headers: {
     authorization: token ? `Bearer ${token}` : '',
